0985497ef1085e0fe32312868e6dd38c193b491f
villageworks <villageworks@gmail.com>
2025-05-17 23:04:11 +0900

    ver.0.60: step6

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -26,30 +26,35 @@ public class ItemController {
 	 */
 	@GetMapping("/items")
 	public String index(
+			@RequestParam(name = "keyword", defaultValue = "") String keyword,
 			@RequestParam(name = "sort", defaultValue = "") String sort,
 			@RequestParam(name = "maxPrice", defaultValue = "") Integer maxPrice,
 			@RequestParam(name = "categoryId", defaultValue = "0") int categoryId,
 			Model model) {
 		// 【step3での変更箇所】価格上限値によるデータベースから上限値以下の価格の商品の商品リストを取得
 		List<Item> list = null;
-		
-		if (maxPrice != null ) {
-			// 価格上限値による絞り込み検索
-			list = itemRepository.findByPriceLessThanEqual(maxPrice);
-			// 【step5での変更箇所】価格上限値を遷移先画面に受け継ぐ
-			model.addAttribute("maxPrice", maxPrice);
+		if (!keyword.isEmpty()) {
+			list = itemRepository.findByNameContains(keyword);
+			model.addAttribute("keyword", keyword);
 		} else {
-			// 【step4での変更箇所】データベースから「安い順」に並べ替えた商品リストを取得
-			if (sort.equals("priceAsc")) {
-				list = itemRepository.findAllByOrderByPrice();
+			if (maxPrice != null ) {
+				// 価格上限値による絞り込み検索
+				list = itemRepository.findByPriceLessThanEqual(maxPrice);
+				// 【step5での変更箇所】価格上限値を遷移先画面に受け継ぐ
+				model.addAttribute("maxPrice", maxPrice);
 			} else {
-				// 【step2での変更箇所】カテゴリコードによるデータベースからすべての商品の商品リストを取得
-				if (categoryId == 0) {
-					// 全商品検索の場合
-					list = itemRepository.findAll();
+				// 【step4での変更箇所】データベースから「安い順」に並べ替えた商品リストを取得
+				if (sort.equals("priceAsc")) {
+					list = itemRepository.findAllByOrderByPrice();
 				} else {
-					// カテゴリ検索（絞り込み検索）の場合
-					list = itemRepository.findByCategoryId(categoryId);
+					// 【step2での変更箇所】カテゴリコードによるデータベースからすべての商品の商品リストを取得
+					if (categoryId == 0) {
+						// 全商品検索の場合
+						list = itemRepository.findAll();
+					} else {
+						// カテゴリ検索（絞り込み検索）の場合
+						list = itemRepository.findByCategoryId(categoryId);
+					}
 				}
 			}
 		}
