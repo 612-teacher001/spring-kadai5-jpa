d3ebf28016a9dcbea658a521a8a1de62acedd148
villageworks <villageworks@gmail.com>
2025-05-17 22:01:00 +0900

    ver.0.20: step2

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
 import com.example.demo.repository.ItemRepository;
@@ -17,13 +18,24 @@ public class ItemController {
 	ItemRepository itemRepository;
 	
 	/**
-	 * Step1：商品一覧画面を表示する
+	 * Step2：カテゴリ別商品検索一覧画面を表示する
+	 * @param categoryId 検索対象商品のカテゴリコード
+	 * @param model 共用のデータ置き場
 	 * @return 遷移先ThymeLeafテンプレート名
 	 */
 	@GetMapping("/items")
-	public String index(Model model) {
-		// データベースからすべての商品の商品リストを取得
-		List<Item> list = itemRepository.findAll();
+	public String index(
+			@RequestParam(name = "categoryId", defaultValue = "0") int categoryId,
+			Model model) {
+		// 【step2での変更箇所】カテゴリコードによるデータベースからすべての商品の商品リストを取得
+		List<Item> list = null;
+		if (categoryId == 0) {
+			// 全商品検索の場合
+			list = itemRepository.findAll();
+		} else {
+			// カテゴリ検索（絞り込み検索）の場合
+			list = itemRepository.findByCategoryId(categoryId);
+		}
 		// 商品リストをデータ置き場に登録
 		model.addAttribute("items", list);
 		// 画面遷移
